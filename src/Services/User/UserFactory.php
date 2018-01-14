b4d8c4a253812511bd1699c4003114f96e8736cd
Paulius Janciukas <paulius@versliukai.lt>
2018-01-14 21:24:09 +0200

    added user type to user factory service

--- src/Services/User/UserFactory.php
+++ src/Services/User/UserFactory.php
@@ -1,2 +1,70 @@
 <?php
-namespace 
+namespace Paysera\Services\User;
+
+use Paysera\Classes\Transaction;
+use Paysera\Classes\User;
+
+class UserFactory
+{
+    public function buildFromData($data)
+    {
+        return new User(
+          $data['id'],
+          $data['type']
+        );
+    }
+
+    /**
+     * @param Transaction[] $transactions
+     * @return array
+     */
+    public function buildFromTransactions(array $transactions) {
+        $uniqueUsers = $this->findUniqueUserIds($transactions);
+        $users = [];
+
+        foreach ($uniqueUsers as $key => $uniqueUser) {
+            $userTransactions = $this->findTransactionsByUserId($uniqueUser['id'], $transactions);
+            $users[$key] = $this->buildFromData($uniqueUser);
+            $users[$key]->setUserTransactions($userTransactions);
+        }
+
+        return $users;
+
+    }
+
+    /**
+     * @param int $userId
+     * @param Transaction[] $transactions
+     * @return Transaction[]
+     */
+    private function findTransactionsByUserId($userId, $transactions)
+    {
+        $userTransactions = [];
+
+        foreach ($transactions as $transaction)
+        {
+            if ($transaction->getUserId() == $userId) {
+                $userTransactions[] = $transaction;
+            }
+        }
+        return $userTransactions;
+    }
+
+    /**
+     * @param Transaction[] $transactions
+     * @return array
+     */
+    private function findUniqueUserIds($transactions)
+    {
+        $uniqueUsers = [];
+
+        foreach ($transactions as $key => $transaction) {
+            if(!in_array($transaction->getUserId(), array_column($uniqueUsers, 'id'))) {
+                $uniqueUsers[$key]['id'] = $transaction->getUserId();
+                $uniqueUsers[$key]['type'] = $transaction->getUserType();
+            }
+        }
+
+        return $uniqueUsers;
+    }
+}
